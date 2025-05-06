ec86fa153b6f8aef3cfa9ae4ecc5a69699c3e123
Hazko-stack <111404022+Hazko-stack@users.noreply.github.com>
2025-05-06 17:05:23 +0700

    Update firedetector.ino

--- firedetector.ino
+++ firedetector.ino
@@ -1,45 +1,44 @@
 #define BLYNK_TEMPLATE_ID "TMPL66YX1Cb1d"
-#define BLYNK_TEMPLATE_NAME "FIre Detector"
+#define BLYNK_TEMPLATE_NAME "Fire Detector"
 #define BLYNK_AUTH_TOKEN "0UyQY9t1dA7nbEi_5lox85pg4tzNXTvv"
 
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 #define BLYNK_PRINT Serial
 
-int mq2 = 34;
-int buzzer = 13;
-int gasThreshold = 1000;
+int flameSensor = D6;   
+int buzzer = D5;       
 
-char ssid[] = "Wokwi-GUEST"; 
-char pass[] = "";
+char ssid[] = "Home1"; 
+char pass[] = "cucidulu";
+
 BlynkTimer timer;
+
+void checkFlame() {
+  int flameValue = digitalRead(flameSensor);
+  Serial.print("Flame Detected: ");
+  Serial.println(flameValue == LOW ? "YES" : "NO");
+
+  if (flameValue == LOW) {
+    digitalWrite(buzzer, HIGH);         // Nyalakan buzzer
+    Blynk.virtualWrite(V0, 1);          // Kirim status api = ada
+  } else {
+    digitalWrite(buzzer, LOW);          // Matikan buzzer
+    Blynk.virtualWrite(V0, 0);          // Kirim status api = aman
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(mq2, INPUT);
+  pinMode(flameSensor, INPUT);
   pinMode(buzzer, OUTPUT);
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  timer.setInterval(500L, checkFlame); // Cek setiap 500ms
 }
 
 void loop() {
   Blynk.run();
-
-  int gasValue = analogRead(mq2);
-  Serial.print("Gas Value: ");
-  Serial.println(gasValue);
-
-  if (gasValue > gasThreshold) {
-    digitalWrite(buzzer, HIGH);
-    Blynk.virtualWrite(V0, 1);
-  } else {
-    digitalWrite(buzzer, LOW);
-    Blynk.virtualWrite(V0, 0);
-  }
-
-  Blynk.virtualWrite(V1, gasValue);
-
-  delay(500);
+  timer.run();
 }
